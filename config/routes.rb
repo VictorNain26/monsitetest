82e8619c3244dfc2395b4e2f8396146e9d8123b2
VictorNain26 <victor.lenain26@gmail.com>
2021-11-14 01:20:20 +0100

    add action cable chat fixe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
-  mount ActionCable.server => '/tchatboxs'
   resources :tchatboxs, only: %i[index create]
   resources :sinemas, only: %i[index]
 end
